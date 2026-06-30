c95e846fb95bc72d427ca9fda910065c706e8c24
Milagros <milagros.rs289@gmail.com>
2026-06-30 18:02:27 -0500

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://cnt-68466a5a-fdfd-44d9-9bec-cdb68a8df54c.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+CREATE_USER_PATH = "/api/v1/users/"
